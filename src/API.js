08eb260eeb59add5202986e96267d53ed507bf1f
Sarveshwar Biradar <sarveshwar@mobikontech.com>
2018-08-17 23:38:55 +0530

    pagination and header

--- src/API.js
+++ src/API.js
@@ -1,6 +1,6 @@
-export const getAllCoins = (currency='USD',limit=30,offset=0,sort='coinranking') =>{
+export const getAllCoins = (currency='USD',limit=30,offset=0,sort='coinranking',order='desc') =>{
     return(
-        fetch(`https://api.coinranking.com/v1/public/coins?base=${currency}&limit=${limit}&offset=${offset}&sort=${sort}`)
+        fetch(`https://api.coinranking.com/v1/public/coins?base=${currency}&limit=${limit}&offset=${offset}&sort=${sort}&order=${order}`)
             .then(res => res.json())
             .then(data => data.data)
             .catch(e=>{
